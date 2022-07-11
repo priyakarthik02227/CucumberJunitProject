48acfe96cf5a3bcfa0eb32ffdca1d3d4021e262e
Lakshmipriya <priyakarthik02227@gmail.com>
2022-07-11 10:09:31 +0530

    Added tags

--- src/test/java/com/vcentry/lab/runner/TestRunner.java
+++ src/test/java/com/vcentry/lab/runner/TestRunner.java
@@ -10,9 +10,9 @@ features="src/test/java/com/vcentry/lab/features",
 glue="com.vcentry.lab.stepDefinition",	//package name of step definition	
 plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 monochrome=true, //for readable console  //by default monochrome is false
-dryRun=false //dry run used to check feature file steps were implemented in test steps 
+dryRun=false, //dry run used to check feature file steps were implemented in test steps 
 //tags="@validlogin or @sanity"
-//tags= "@sanity"  
+tags= "@sanity"  
 
 		)
 
